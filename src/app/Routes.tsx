3ee6b0059f2054cfe01b5a8742d32aab29a75a15
Mehdi Djendar <mehdi.djendar@epitech.eu>
2023-06-17 05:44:37 +0200

    feat: login page

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes as RouterRoutes } from 'react-router-dom';
 import Login from '../pages/Login';
+import { Box, Image, Flex, Text, Stack, Input } from '@chakra-ui/react'
 
 const Routes = (): JSX.Element => (
 	<BrowserRouter>
