06eb68f7d11021e88dc15bde90644260473ce4d2
Mehdi Djendar <mehdi.djendar@epitech.eu>
2023-06-19 21:13:41 +0200

    fix: file create react app

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,45 +1,65 @@
-import { Component } from 'react';
-import login_image from '../ressource/login_image.jpg';
-import logo from '../ressource/logo.png';
-import { Box, Image, Flex, Text, Stack, Input, Button } from '@chakra-ui/react'
-import { FcGoogle } from 'react-icons/fc';
+import login_image from "../ressource/login_image.jpg";
+import logo from "../ressource/logo.png";
+import { FcGoogle } from "react-icons/fc";
 
-export default class Login extends Component {
-  render() {
-    return (
-      <Flex direction="row" h="100vh">
-        <Box w={['100%', '50%']} display="flex" flexDirection="column" alignItems="center" m={'50'}>
-          <Image src={logo} alt="logo" w="18%" h="7%" />
-          <Stack mt={'50'} >
-            <Text textAlign="center" fontSize="6xl" fontWeight="bold">
-              Welcome-back
-            </Text>
-            <Text textAlign="center" color="grey">
-              Log in to manage your projects, they will grow up
-            </Text>
-            <Text mb='8px' mt="50px">Email:</Text>
-            <Input variant='flushed' size="lg" placeholder='Enter your email...' />
-            <Text mb='8px' mt="50px">Password:</Text>
-            <Input variant='flushed' size="lg" placeholder='Enter your password...' />
-            <Text fontSize='xs' color="grey">
-              Forgot password
-            </Text>
-            <Button mt="50px" colorScheme="gray" size="lg">
-              Sign in
-            </Button>
-            <Button
-              w={'full'}
-              maxW={'md'}
-              variant={'outline'}
-              leftIcon={<FcGoogle />}>
-                <Text>Sign in with Google</Text>
-            </Button>
-          </Stack>
-        </Box>
-        <Box w={['100%', '50%']} h="100%" display="flex">
-          <Image src={login_image} alt="login_image" w="100%" h="100%" />
-        </Box>
-      </Flex>
-    );
-  }
+export default function Login() {
+  return (
+    <>
+      <div className="flex bg-white flex-col h-screen lg:flex-row">
+        <div className="flex-grow w-1/2 bg-white card rounded-box place-items-center">
+          <img src={logo} alt="logo" className="h-20 w-50 pt-10" />
+          <h1 className="text-6xl font-bold text-black pt-20">
+            Welcome to Triomphy
+          </h1>
+          <p className="pt-3">
+            {" "}
+            Log in for manage yout project, they will grow up !
+          </p>
+          <div className="form-control w-full max-w-xl mt-20">
+            <label className="label">
+              <span className="label-text text-black"> Email </span>
+            </label>
+            <input
+              type="text"
+              placeholder="Type your email ..."
+              className="input input-ghost border-0 border-b-2 rounded-none border-gray-200 bg-transparent focus:outline-none focus:text-black"
+            />
+          </div>
+          <div className="form-control w-full max-w-xl mt-10">
+            <label className="label">
+              <span className="label-text text-black"> Password </span>
+            </label>
+            <input
+              type="text"
+              placeholder="Type your password ..."
+              className="input input-ghost border-0 border-b-2 rounded-none border-gray-200 bg-transparent focus:outline-none focus:text-black"
+            />
+            <label className="label">
+              <span className="label-text-alt text-black">
+                {" "}
+                Forget password{" "}
+              </span>
+            </label>
+          </div>
+          <div className="form-control w-full max-w-xl mt-10">
+            <button className="btn btn-active text-white"> Sign in </button>
+          </div>
+          <div className="form-control w-1/2 max-w-xl mt-10">
+            <button className="btn btn-active text-black border-black border-1 bg-white focus:bg-transparent">
+              <FcGoogle />
+              Sign in with Google
+            </button>
+          </div>
+        </div>
+
+        <div className="flex-grow w-1/2 bg-white card rounded-box place-items-end">
+          <img
+            src={login_image}
+            alt="login_image"
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
