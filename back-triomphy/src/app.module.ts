be55fe4e632beda8a4aa4002cdebce9ea0eb860e
Mehdi Djendar <mehdi.djendar@epitech.eu>
2023-06-29 00:07:19 +0200

    feat: back-end with some route

--- back-triomphy/src/app.module.ts
+++ back-triomphy/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserSchema } from './schema/user.schema';
+import { UserController } from './controller/user/user.controller';
+import { UserService } from './providers/user/user.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    // MongooseModule.forRoot(databse_url),
+    // MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ back-triomphy/src/config/configuration.ts
@@ -0,0 +1,3 @@
+export default () => ({
+  database_url: process.env.DATABASE_URL,
+});
